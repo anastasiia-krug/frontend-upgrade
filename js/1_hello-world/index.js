93c5ed9be8428366111a4dd1a53d80159f0fd974
Anastasiia Krug <nastasiia.krug@gmail.com>
2025-06-30 22:47:18 +0300

    JS: 1. Hello World - задания 2, 3, 4

---
+++ js/1_hello-world/index.js
@@ -0,0 +1,20 @@
+// Задание 2
+
+console.log('Запускаем таймер');
+
+alert('5');
+alert('4');
+alert('3');
+alert('2');
+alert('1');
+alert('0');
+
+console.log('Обратный отсчет закончен');
+
+// Задание 3
+
+alert('Брендан Айк (Эйх) разработал JavaScript');
+alert('JavaScript был разработан за 10 дней');
+alert('JavaScript появился в 1995 году');
+alert('JavaScript — это не Java');
+alert('EcmaScript является спецификацией JavaScript');
