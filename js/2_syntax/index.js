137b68e67e2430a649cf60e134a543d68c72a91e
Anastasiia Krug <nastasiia.krug@gmail.com>
2025-07-02 23:27:52 +0300

    JS: 1. Hello World - добавила задание 4, 2. Синтаксис - задания 1, 2, 3, 4, 5

---
+++ js/2_syntax/index.js
@@ -0,0 +1,65 @@
+// Задания 1 и 2
+
+// JavaScript-код с созданием переменных
+
+const myName = 'Anastasiya';
+const mySurname = 'Krug';
+const myFavoriteDrink = 'Tea';
+const myFavoriteAnimal = 'Cat';
+const myFavoriteProgrammingLanguage = 'JavaScript';
+
+console.log(myName);
+console.log(mySurname);
+console.log(myFavoriteDrink);
+console.log(myFavoriteAnimal);
+console.log(myFavoriteProgrammingLanguage);
+
+/* Автор: 
+Анастасия Круг */
+
+
+// Задание 3
+
+const carName = 'Ford Focus';
+let maxCarSpeed = 180;
+let carOwner = 'Иван';
+
+alert(carName);
+alert(maxCarSpeed);
+alert(carOwner);
+
+
+// Задание 4
+
+maxCarSpeed = 185;
+carOwner = 'Николай';
+
+console.log('У машины изменилась скорость', maxCarSpeed);
+console.log('У машины изменился владелец', carOwner);
+
+maxCarSpeed = 190;
+carOwner = 'Полина';
+
+console.log('У машины изменилась скорость', maxCarSpeed);
+console.log('У машины изменился владелец', carOwner);
+
+maxCarSpeed = 195;
+carOwner = 'Анастасия';
+
+console.log('У машины изменилась скорость', maxCarSpeed);
+console.log('У машины изменился владелец', carOwner);
+
+
+// Задание 5
+
+const question1 = '1. Какая самая маленькая страна мира по населению?';
+const question2 = '2. Какая самая большая страна мира по населению?';
+const question3 = '3. Какой самый большой материк на планете?';
+
+const answer1 = 'Китай';
+const answer2 = 'Евразия';
+const answer3 = 'Ватикан';
+
+console.log(question1, 'Ответ:', answer3);
+console.log(question2, 'Ответ:', answer1);
+console.log(question3, 'Ответ:', answer2);
